cc8535e5eac8525a10661f4812d8ea96a766273e
Felipe Kauãn <felipekauan30@outlook.com>
2026-02-22 13:30:07 -0300

    feat: integra model Usuario ao HomeController

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,9 +1,13 @@
 <?php
 require_once __DIR__ . '/../core/Controller.php';
+require_once __DIR__ . '/../models/Usuario.php';
 
 class HomeController extends Controller {
     public function index() {
-        $this->view('home/index');
+        $usuario = new Usuario();
+        $data = $usuario->getUserData();
+        
+        $this->view('home/index', $data);
     }
 }
 
