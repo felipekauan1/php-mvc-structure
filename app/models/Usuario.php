a0ba3809b28160d1d5116e4c9a4b042023a6846f
Felipe Kauãn <felipekauan30@outlook.com>
2026-02-22 13:29:43 -0300

    feat: adiciona model Usuario para simulação de dados

---
+++ app/models/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+class Usuario {
+    public function getUserData() {
+        // simulando dados de usuarios
+        return [
+            'nome' => 'Felipe Kauãn',
+            'idade' => 20,
+            'email' => 'felipe@gmail.com'
+        ];
+    }
+}
+
+?>
