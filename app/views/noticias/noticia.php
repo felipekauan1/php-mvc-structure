1cdab87184ec3d89162af1b9afdb9f95bb8f4ce2
Felipe Kauãn <felipekauan30@outlook.com>
2026-02-23 16:29:29 -0300

    feat: adiciona view dinâmica para exibição de notícia por id

---
+++ app/views/noticias/noticia.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Noticia</title>
+</head>
+<body>
+    <h1>Noticia</h1>
+    <p>Texto da noticia número: <?= $idNoticia ?></p>
+</body>
+</html>
